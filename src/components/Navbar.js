5e5f5be1524ae98accb3cff179e7444d873c2bb8
masker-dev <maskerbos@gmail.com>
2022-12-31 10:11:45 +0200

    last commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,10 +9,16 @@ const Navbar = () => {
   const myHeader = useRef();
   const navMenu = useRef();
   const [isOpen, setIsOpen] = useState(true);
-  window.onload = () => {
+  const addcss = () => {
     let secWidth = window.innerWidth;
     if (secWidth < 768) myHeader.current.classList.add("mobileView");
     else myHeader.current.classList.remove("mobileView");
+  };
+  window.onload = () => {
+    addcss();
+  };
+  window.onresize = () => {
+    addcss();
   };
   window.onscroll = () => {
     myHeader.current.classList.toggle("stikky", window.scrollY > 120);
